54db6e733f24446976d8d27f5f91cd047210edf6
Shoxrux <80318006+shoxa2006@users.noreply.github.com>
2022-07-13 10:37:43 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+    // for(let i = 1 ; i <= p ; i+=2){
+    //     console.log(i)
+    // }
+
+    // let num = p;
+    // let flag = true;
+    // for (let i = 2; i < num; i++) {
+	//     if (num % i == 0) {
+	// 	    flag = false;
+	// 	    break;
+	//     }
+//     let n = p;
+
+// nextPrime:
+// for (let i = 2; i <= n; i++) {
+
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) continue nextPrime;
+
+//   }
+
+//   console.log( i );
+
+// }
